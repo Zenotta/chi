cf5638d72247978cf53310f163c2ab02b58f3c78
Byron Houwens <zhion360@gmail.com>
2021-05-16 20:05:51 +0200

    Inlining arrow SVG

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -3,7 +3,6 @@ import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
 
 import styles from './Table.scss';
-const arrowIcon = require('../../images/arrow.svg');
 
 export interface TableCell {
     id?: string,
@@ -29,6 +28,19 @@ interface HeaderId {
 export const Table = (props: TableProps) => {
     const [order, setOrder] = useState(props.order || 'asc');
     const headerIds: HeaderId[] = [];
+
+
+    const getArrowIcon = () => {
+        return (
+            <svg className={`${styles.arrow} ${styles[`direc-${order ? order : 'desc'}`]}`} x="0px" y="0px" viewBox="0 0 492.004 492.004">
+                <path d="M484.14,226.886L306.46,49.202c-5.072-5.072-11.832-7.856-19.04-7.856c-7.216,0-13.972,2.788-19.044,7.856l-16.132,16.136
+			c-5.068,5.064-7.86,11.828-7.86,19.04c0,7.208,2.792,14.2,7.86,19.264L355.9,207.526H26.58C11.732,207.526,0,219.15,0,234.002
+			v22.812c0,14.852,11.732,27.648,26.58,27.648h330.496L252.248,388.926c-5.068,5.072-7.86,11.652-7.86,18.864
+			c0,7.204,2.792,13.88,7.86,18.948l16.132,16.084c5.072,5.072,11.828,7.836,19.044,7.836c7.208,0,13.968-2.8,19.04-7.872
+			l177.68-177.68c5.084-5.088,7.88-11.88,7.86-19.1C492.02,238.762,489.228,231.966,484.14,226.886z"/>
+            </svg>
+        );
+    }
 
     /** 
      * Maps an input value to a generated ID
@@ -128,7 +140,7 @@ export const Table = (props: TableProps) => {
         const stabilizedThis: any[] = body.map((el, index) => [el, index]);
         stabilizedThis.sort((a: any, b: any): any => {
             const order = comparator(a[0], b[0]);
-            
+
             if (order !== 0) return order;
             return a[1] - b[1];
         });
@@ -158,7 +170,7 @@ export const Table = (props: TableProps) => {
                                         key={heading.id}
                                         align={alignment}>
                                         {props.sortable &&
-                                            <div 
+                                            <div
                                                 className={`${styles.sorter} ${orderBy == heading.id ? styles.active : ''}`}
                                                 onClick={() => handleRequestSort(heading.id)}>
                                                 {heading.value}
@@ -167,11 +179,7 @@ export const Table = (props: TableProps) => {
                                                         {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
                                                     </span>
                                                 ) : null}
-                                                <img 
-                                                    src={arrowIcon} 
-                                                    alt="sort arrow" 
-                                                    className={`${styles.arrow} ${styles[`direc-${order ? order : 'desc'}`]}`} 
-                                                />
+                                                {getArrowIcon()}
                                             </div>}
                                         {!props.sortable && heading.value}
                                     </th>
